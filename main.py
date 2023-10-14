9c0e8c33012b9ddb617c4ca01e8e82f73949657c
xingjian <xingjian@baichuan-inc.com>
2023-10-14 20:58:29 +0800

    new port

--- main.py
+++ main.py
@@ -42,4 +42,4 @@ async def create_chat_completion(body: ChatCompletionBody):
 
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host='0.0.0.0', port=8000, reload=True)
+    uvicorn.run("main:app", host='localhost', port=31014, reload=True)
